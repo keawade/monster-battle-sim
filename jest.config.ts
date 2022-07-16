10c788a366850eec40286e50b8fd47e6053ef816
Keith Wade <keawade@protonmail.com>
2022-07-16 15:46:09 -0400

    Update jest config to test typescript

--- jest.config.ts
+++ jest.config.ts
@@ -2,9 +2,11 @@ import type { Config } from '@jest/types';
 
 // Sync object
 const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   verbose: true,
   collectCoverage: true,
-  testMatch: ['test/**/*.spec.ts'],
+  testMatch: ['**/test/**/*.spec.ts'],
 };
 
 export default config;
